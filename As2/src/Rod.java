a64a4f41d55d9b073fc8c4d3f1c16c8a529cae41
415366ru <415366ru@T03-0051-1018.campus.eur.nl>
2018-09-27 16:12:07 +0200

    Piece and Rod classes

--- As2/src/Rod.java
+++ As2/src/Rod.java
@@ -4,5 +4,25 @@
  *
  */
 public class Rod {
+	
+	private final int LENGTH;
+	
+	/**
+	 * Constructor
+	 * @param length of the rod
+	 */
+	public Rod(int length) {
+		this.LENGTH = length;
+	}
+	
+	// getter
+	public int getLength() {
+		return this.LENGTH;
+	}
+	
+	// toString
+	public String toString() {
+		return " " + this.LENGTH;
+	}
 
 }
