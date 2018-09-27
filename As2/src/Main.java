932593179b1ce7f19ff7d7e005587c247d337d57
69208pdv <69208pdv@T03-0051-1017.campus.eur.nl>
2018-09-27 16:10:56 +0200

    Main reads small.txt

--- As2/src/Main.java
+++ As2/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main 
 {
@@ -5,7 +8,28 @@ public class Main
 	public static void main(String[] args)
 	{
 		//main method here
-		
+		final int NR_PIECES = readFile(new File("small.txt"));
+		System.out.println(NR_PIECES);
+	}
+
+	private static int readFile(File file) {
+		int nrPieces = 0;
+		try
+		{
+			Scanner s = new Scanner(file);
+			while (s.hasNextLine())
+			{
+				int index = s.nextInt();
+				int length = s.nextInt();
+				Piece p = new Piece(index, length);
+				nrPieces++;
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return nrPieces;
 	}
 	
 }
