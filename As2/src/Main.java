8dbb7f95ff2c256fffad55b5e79bf3aa5f1f3f3e
reinier <reinier@192.168.1.58>
2018-09-28 16:12:44 +0200

    Added generation of feasible patterns to Main

--- As2/src/Main.java
+++ As2/src/Main.java
@@ -1,8 +1,10 @@
-import java.util.List;
-import java.util.Map;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 
 import ilog.concert.IloException;
@@ -33,6 +35,63 @@ public class Main
 		// print solution info
 		System.out.println("Objective: " + model.getObjective());
 		printSolutionInfo(model);
+		
+		// create all possible patterns
+		List<Pattern> patterns = createFeasiblePatterns(allPieces, ROD_LENGTH);
+		System.out.println(patterns.size()); // zelfde als Pim
+	}
+	
+	/**
+	 * Creates a list of all feasible patterns, for the given list of pieces.
+	 * @return
+	 */
+	private static List<Pattern> createFeasiblePatterns(List<Piece> pieces, int MAX_LENGTH) {
+		List<Pattern> result = new ArrayList<Pattern>();
+		Queue<Pattern> q1 = new LinkedList<Pattern>();
+		Queue<Pattern> q2 = new LinkedList<Pattern>();
+		// initialize q1 with an empty pattern and a pattern with the first piece
+		Pattern p1 = new Pattern(1,new ArrayList<Piece>());
+		List<Piece> listP2 = new ArrayList<Piece>();
+		listP2.add(pieces.get(0));
+		Pattern p2 = new Pattern(2,listP2);
+		q1.add(p1);
+		q1.add(p2);
+		int counter = 2;			// count the number of patterns: needed for the index
+		// Now generate all possible patterns
+		for (int i=1; i<pieces.size(); i++) {
+			// look which queue is empty
+			Queue<Pattern> empty = null;
+			Queue<Pattern> full = null;
+			if (q1.isEmpty()) {
+				empty = q1;
+				full = q2;
+			} else {
+				empty = q2;
+				full = q1;
+			}
+			// retrieve the patterns from the full queue. Generate new ones and add the old ones
+			while (!full.isEmpty()) {
+				counter++;
+				Pattern current = full.poll();
+				List<Piece> currentPieces = current.getPieces();
+				empty.add(current);
+				// generate the new pattern
+				List<Piece> newPatternList = new ArrayList<Piece>();
+				newPatternList.addAll(currentPieces);
+				newPatternList.add(pieces.get(i));
+				Pattern newPattern = new Pattern(counter, newPatternList);
+				// check if this pattern is feasible
+				if (newPattern.totalLength() <= MAX_LENGTH) {
+					empty.add(newPattern);
+				} else {
+					counter--; // adjust the counter
+				}
+			}
+		}
+		// determine which queue is full now and store in result
+		if (!q1.isEmpty()) {result.addAll(q1); }
+		else {result.addAll(q2); }
+		return result;
 	}
 	
 	/**
---
+++ As2/src/Pattern.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+/**
+ * Pattern object for the Cutting Stock problem. Contains a list which pieces are in this pattern.
+ * @author Peter de Voogd and Reinier van Uden
+ *
+ */
+public class Pattern {
+	
+	private List<Piece> pieces;
+	private int index;
+	
+	// constructor
+	public Pattern(int index, List<Piece> pieces) {
+		this.index = index;
+		this.pieces = pieces;
+	}
+	
+	// getters
+	public List<Piece> getPieces() {
+		return this.pieces;
+	}
+	
+	public int getIndex() {
+		return this.index;
+	}
+	
+	public int totalLength() {
+		int result = 0;
+		for (Piece p : pieces) {
+			result = result + p.getLength();
+		}
+		return result;
+	}
+	
+	// print method
+	public String toString() {
+		String result = index + "[";
+		for (Piece p : pieces) {
+			result = result + p.getIndex() + ",";
+		}
+		result = result + "]";
+		return result;
+	}
+}
